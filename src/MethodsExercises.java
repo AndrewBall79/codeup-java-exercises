c83153513ee9437674c901c21e6a7dd890bcb73f
Andrew Ball <andy.bob.ball@gmail.com>
2020-03-24 20:47:10 -0500

    tuesday night

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,22 +1,46 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MethodsExercises {
     public static void main(String[] args) {
-//        int base = 1;
-//        int exponent = 7;
-//        int add = base + exponent;
-//        System.out.println("The base plus the exponent is: " + add);
-//        int sub = base - exponent;
-//        System.out.println("The base minus the exponent is: " + sub);
-//        int times = base * exponent;
-//        System.out.println("The base times the exponent is: " + times);
-//        int divide = base / exponent;
-//        System.out.println("The base divided by the exponent is: " + divide);
-//        int modulus = base % exponent;
-//        System.out.println("The base modulus the exponent is: " + modulus);
-//
+
+//        public static int add(int a, int b){ return a+ b;}
+        int base = 1;
+        int exponent = 7;
+        int add = base + exponent;
+        System.out.println("The base plus the exponent is: " + add);
+        int sub = base - exponent;
+        System.out.println("The base minus the exponent is: " + sub);
+        int times = base * exponent;
+        System.out.println("The base times the exponent is: " + times);
+        int divide = base / exponent;
+        System.out.println("The base divided by the exponent is: " + divide);
+        int modulus = base % exponent;
+        System.out.println("The base modulus the exponent is: " + modulus);
+
 
         Scanner input = new Scanner(System.in);
+
+//public static int multWithLoop(int a, int b){
+//    int j = a;
+//    if (a<0) b = 0 - b;
+//    int product = 0;
+//    for (int i = a; i != 0; i -= j){
+//        product += b;
+//    }
+//    return product;
+//        }
+
+//        public static int multWithRecursion(int a, int b){
+//            int product = 0;
+//            if (a> 0){
+//                product = b + multWithRecursion(a-1,b);
+//            }else if (a<0){
+//                product = (0-b) + multWithRecursion(a+1, b);
+//            }
+//            return product;
+//        }
+//
 //        boolean cContinue = true;
 //        do {
 //            System.out.println("Please enter a number between 1 and 10: ");
@@ -29,23 +53,58 @@ public class MethodsExercises {
 //            }
 //        } while (cContinue) ;
 //    System.out.println("No sorry that's not right");
+//    }
+//
+//
+//public static int getInteger(int min, int max) {
+//    scanner = new Scanner(System.in);
+//    boolean doAgain = true;
+//    int num = 0;
+//    do {
+//        try {
+//            num = scanner.nextInt();
+//            if (num < min || num > max) {
+//                System.out.printf("\nThe integer %d is out of range\n", num);
+//                System.out.printf("Enter an integer beween %d and %d ", min, max);
+//
+//            } else {
+//                doAgain = false;
+//            }
+//        } catch (InputMismatchException ime) {
+//            System.out.println("That is not an integer.");
+//            System.out.printf("Enter an integer beween %d and %d ", min, max);
+//            scanner.nextLine();
+//            num = 0;
+//        }
+//    } while (doAgain);
+//    return num;
+//}
 
-        boolean cContinue = true;
-        do {
-            System.out.println("Please enter a number between 1 and 10: ");
-            int userNumber = input.nextInt();
-            System.out.println("You Entered: "+ userNumber);
-            if (userNumber < 1 || userNumber > 10) {
-                cContinue = false;
-            }else {
-                System.out.println("You Know Your Way Around A Keyboard!");
-            }
-        } while (cContinue) ;
-    System.out.println("No sorry that's not right");}
 
-
+//public static long calcFactorial(int num) throws ArithmeticException{
+//    long factorial = 1;
+//    long previousFactorial = 1;
+//    for (int i = 1; i <= num; ++i){
+//        factorial *= i;
+//        if (previousFactorial > factorial);
+//    }
+//        }
+//
+//public static long recursiveCalcFactorial(int num) throws ArithmeticException{
+//    if (num <=1) return; 1;
+//    long fact = num * recursiveCalcFactorial(num-1)
+//        }
+//
+//
+//
+//
+//public static Scanner scanner;
+//
+//
+//
+//}
+    }
 }
-
 
 
 
--- src/Practice.java
+++ src/Practice.java
@@ -1,15 +1,60 @@
 public class Practice {
-    public static void main (String[] args){
-        int num1 = 11;
-        int answer;
+    public static void main(String[] args) {
+//        int num1 = 7;
+//        int num2 = 78;
+//
+//        int answer1;
+//        int answer2;
+//        boolean answer3;
+//
+//        answer1 = multNumbers(num1);
+//        System.out.println("When we multiply num1 by 7 we get: " + answer1);
+//        answer2 = addNumbers(num1, num2);
+//        System.out.println("When we add num1 and num2, we get: " + answer2);
+//
+//        answer3 = equalNumbers(num1, num2);
+//        System.out.println("num1 and num2 are equal: " + answer3);
+//    }
+//    public static int multNumbers(int a){
+//        return a * 7;
+//    }
+//
+//    public static int addNumbers(int x, int y){
+//        return x + y;
+//    }
+//    public static boolean equalNumbers(int r, int t){
+//        return r == t;
 
-        answer = multNumbers(num1);
+        int var1 = 5;
+        double var2 = 3.65;
 
-        System.out.println("The Result is: " + answer);
+        aircraft cessna172 = new aircraft();    //creates object of class aircraft
+
+        aircraft piperSaratoga = new aircraft(); //creates object contains all class variables
+
+        aircraft g4 = new aircraft(); //
+
+        cessna172.passengers = 4;
+        cessna172.cruiseSpeed = 140;
+        cessna172.fuelCapacity = 56.5;
+        cessna172.fuelBurnRate = 9.5;
+
+        System.out.println("Cessna 172 Passengers: "+ cessna172.cruiseSpeed); //
+        System.out.println("Cessna 172 Fuel Burn Rate:" + cessna172.fuelBurnRate + " GPH");
+
+        piperSaratoga.passengers = 6;
+        piperSaratoga.cruiseSpeed = 201;
+        piperSaratoga.fuelCapacity = 102.5;
+        piperSaratoga.fuelBurnRate = 20.5;
+
+        System.out.println("Piper Saratoga Cruise Speed: "+ cessna172.cruiseSpeed + " MPH"); //
+
     }
-    public static int multNumbers(int a){
-        int ans = a * 7;
-        return ans;
-    }
+}
 
+class aircraft {
+    int passengers;       //number of people
+    int cruiseSpeed;      //mph
+    double fuelCapacity;  //gallons
+    double fuelBurnRate;  //gallons per hour
 }
