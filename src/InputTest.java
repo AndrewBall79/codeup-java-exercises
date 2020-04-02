50305b519abd616917f644aa3eee94976ce05b62
Andrew Ball <andy.bob.ball@gmail.com>
2020-04-02 16:43:10 -0500

    EOD Thursday

---
+++ src/InputTest.java
@@ -0,0 +1,20 @@
+import util.Input;
+
+public class InputTest {
+    public static void main(String[] args) throws Exception {
+    Input test1 = new Input();
+    Input.getString("s");
+
+    Input test2 = new Input();
+    Input.yesNo();
+
+    Input test3 = new Input();
+    Input.getInt(1, 10, "s");
+
+    Input test4 = new Input();
+    Input.getInt(1, 10, "s");
+
+    Input test5 = new Input();
+    Input.getDouble(1, 10);
+    }
+}
--- src/util/Input.java
+++ src/util/Input.java
@@ -14,7 +14,7 @@ public class Input {
     }
 
     public static String getString(String s){
-//            System.out.println("Enter something: ");
+            System.out.print("Enter something: ");
             String userInput = scanner.next();
             System.out.println("You entered: --> \"" + userInput + "\" <--");
             return userInput;
@@ -24,21 +24,22 @@ public class Input {
         return true;
     }
 
-    boolean yesNo () {
+    public static boolean yesNo() {
             System.out.println("Enter (y/N): ");
             String userYn = scanner.next();
             boolean confirmation = userYn.equalsIgnoreCase("y");
             System.out.println(confirmation);
             return confirmation;
         }
-        public int getInt(int min, int max, String s){
-            System.out.println("Enter a Number Between 1 and 10");
+        public static int getInt(int min, int max, String s){
             boolean doAgain = true;
             int num = 0;
+            System.out.println("Enter a Number Between 1 and 10");
             do {
                 try {
-                    num = scanner.nextInt();
-                    if (num < min || num > max) {
+                    num = Integer.parseInt(getString(s));
+                    scanner.nextLine();
+                    if (num <= min || num >= max) {
                         System.out.printf("\nThe integer %d is out of range\n", num);
                         System.out.printf("Enter an integer between %d and %d ", min, max);
 
@@ -46,26 +47,26 @@ public class Input {
                         doAgain = false;
                     }
                 } catch (InputMismatchException ime) {
-                    System.out.println("That is not an integer. Plus i had sex during lunch muahahha");
-                    System.out.printf("Enter an integer between %d and %d ", min, max);
+                    System.out.println("That is not an integer.");
                     scanner.nextLine();
                     num = 0;
+
                 }
             } while (doAgain);
             return num;
         }
-//    int getInt(){
+//    public int getInt(){
 //        System.out.println("Enter a Number: ");
 //        int num = scanner.nextInt();
 //        return num;
 //    }
-    double getDouble(double min, double max){
+public static double getDouble(double min, double max){
         System.out.println("Enter a Decimal number Between 1 and 10");
         boolean doAgain = true;
         double num = 0;
         do {
             try {
-                num = scanner.nextInt();
+                num = scanner.nextDouble();
                 if (num < min || num > max) {
                     System.out.printf("\nThe integer %d is out of range\n", num);
                     System.out.printf("Enter an integer beween %d and %d ", min, max);
@@ -83,10 +84,11 @@ public class Input {
         return num;
 
     }
-    double getDouble(){
-        System.out.println("Enter a Decimal Number: ");
-        int num = scanner.nextInt();
-        return num;
-    }
-    }
+//    public double getDouble(){
+//        System.out.println("Enter a Decimal Number: ");
+//        double num = scanner.nextDouble();
+//        return num;
+//    }
+
+}
 
